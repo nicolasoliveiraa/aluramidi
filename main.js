39148a6b564e285552e39dad4cdfe7ce013c8184
nicolasoliveiraa <110068639+nicolasoliveiraa@users.noreply.github.com>
2023-11-22 17:49:43 -0300

    jhg

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-function TocaSomPom(){
+function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick =TocaSomPom;
 
-function TocaSomClap(){
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_clap').onclick =TocaSomClap;
 
-function TocaSomtim(){
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim() {
     document.querySelector('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_tim').onclick =TocaSomTim;
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
 function tocaSomPuff() {
     document.querySelector('#som_tecla_puff').play();
