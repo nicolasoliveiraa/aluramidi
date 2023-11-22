6602e9d1402c01f4b058436d78eace8c103cb64e
nicolasoliveiraa <110068639+nicolasoliveiraa@users.noreply.github.com>
2023-11-22 18:03:59 -0300

    jh,

--- main.js
+++ main.js
@@ -31,3 +31,9 @@ function tocaSomSplash() {
 }
 
 document.querySelector('.tecla_splash').onclick = tocaSomSplash
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim
