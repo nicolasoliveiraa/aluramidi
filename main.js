15e236dedcab409b46b4abbdf70c85d1845d0d86
nicolasoliveiraa <110068639+nicolasoliveiraa@users.noreply.github.com>
2023-11-22 17:52:33 -0300

    fgd

--- main.js
+++ main.js
@@ -14,5 +14,11 @@ function tocaSomTim() {
     document.querySelector('#som_tecla_tim').play();
 }
 
-document.querySelector('.tecla_tim').onclick = tocaSomTim
+document.querySelector('.tecla_puff').onclick = tocaSomTim
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff
 
