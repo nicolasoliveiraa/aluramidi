ccd7739afcfbb4c7eb2c213cad8470d0a7f91007
nicolasoliveiraa <110068639+nicolasoliveiraa@users.noreply.github.com>
2023-11-22 17:57:06 -0300

    ,

--- main.js
+++ main.js
@@ -26,4 +26,4 @@ function tocaSomSplash() {
     document.querySelector('#som_tecla_splash').play();
 }
 
-document.querySelector('.tecla_splash').onclick = tocaSomSplash
+document.querySelector('.tecla_splash').onclick = tocaSomsplash
